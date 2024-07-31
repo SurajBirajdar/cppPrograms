a20dd2f38c1ecf0b65ca9406f115aef9e8b91d7b
SurajBirajdar <78243934+SurajBirajdar@users.noreply.github.com>
2024-08-01 00:57:38 +0530

    Updated arrayLevel1

--- arrayLevel1.cpp
+++ arrayLevel1.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    
     int a = 5;
     cout << "address of a is: " << &a << endl;
     cout << "size of a is: " << sizeof(a) << endl;
@@ -10,6 +11,54 @@ int main() {
     cout << "Base address of arr is: " << &arr << endl;
     cout << "Base address of arr is: " << arr << endl;
 
-    
+    int b[] = {12,13,14,15};
+    for(int i=0; i<4; i++) {
+        cout << b[i] << endl;
+    }
 
+    int brr[5];
+    int size = 5;
+    for(int i=0; i<size; i++) {
+        cout << "Enter the value at index: " << i << "- ";
+        cin >> brr[i];
+        cout << endl;
+    }
+
+    for(int i=0; i<size; i++) {
+        cout << "Value at index: " << i << ": ";
+        cout << brr[i];
+        cout << endl;
+    }
+
+    //Formula for brr[i]
+    //value at (base address + (dataType Size * index))
+
+    int crr[10];
+    int n = 10;
+
+    for(int i=0; i<n; i++) {
+        cin >> crr[i];
+    }
+
+    for(int i=0; i<n; i++) {
+        crr[i] = crr[i] * 2;
+    }
+
+    for(int i=0; i<n ; i++) {
+        cout << crr[i] << " ";
+
+    }
+    
+    int drr[5];
+    int d =5;
+    for(int i=0; i<d; i++) {
+        cin >> drr[i];
+    }
+    cout << endl;
+    int sum = 0;
+    for(int i=0; i<d; i++) {
+        sum = sum + drr[i];
+    }
+
+    cout << "Sum is: " << sum << endl;
 }
