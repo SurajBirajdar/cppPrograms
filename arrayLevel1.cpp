1484e66fc571cc5593f768d4d4758d1bdb271c6c
SurajBirajdar <78243934+SurajBirajdar@users.noreply.github.com>
2024-08-06 01:34:12 +0530

    added linearSearch, countZeroOne, minNumInArray functions

--- arrayLevel1.cpp
+++ arrayLevel1.cpp
@@ -1,9 +1,45 @@
 #include<iostream>
 using namespace std;
 
+    bool linearSearch(int arr[], int size, int target) {
+        for(int i=0; i<size; i++) {
+            if(arr[i] == target) {
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+    
+    // This function will count zero's and one's in array
+    void countZeroOne(int arr[], int size) {
+        int zeroCount = 0;
+        int oneCount = 0;
+        for(int i=0; i<size; i++) {
+            if(arr[i] == 0) {
+                zeroCount++;
+            } else if( arr[i] == 1) {
+                oneCount++;
+            }
+        }
+        cout << "Zero count is: " << zeroCount << endl;
+        cout << "One count is: " << oneCount << endl;
+    }
+
+    //This functions will give minimum number in a given array
+    int minNumInArray(int arr[], int size) {
+        int minAns = INT_MAX;
+        for(int i=0; i<size; i++) {
+            if(arr[i] < minAns) {
+                minAns = arr[i];
+            }
+        }
+        return minAns;
+    }
+
 int main() {
     
-    int a = 5;
+    /*int a = 5;
     cout << "address of a is: " << &a << endl;
     cout << "size of a is: " << sizeof(a) << endl;
 
@@ -60,5 +96,28 @@ int main() {
         sum = sum + drr[i];
     }
 
-    cout << "Sum is: " << sum << endl;
+    cout << "Sum is: " << sum << endl; 
+
+    int zrr[] = {15,20,67,89,35};
+    int size = 5;
+    int target = 36;
+    bool ans = linearSearch(zrr,size, target);
+    if(ans == true) {
+        cout << "Target Found" << endl;
+    } else {
+        cout << "Target not found" << endl;
+    } 
+    
+   int mrr[] = {0,1,0,0,1,1,1,1,0,0,0,1,0};
+   int size = 13;
+   //zero = 7
+   //one = 6
+   countZeroOne(mrr,size); */
+
+   int nrr[] = {67,23,68,10,12};
+   int size = 5;
+   int minNum = minNumInArray(nrr,size);
+   cout << "Minimum number in given array is: " << minNum << endl;
+
+
 }
